e5829962cbc025cb77d0494a292fd4a03b6925d5
adamaubds <150913197+adamaubds@users.noreply.github.com>
2024-09-24 15:33:22 +0100

    s3 bucket - some  of the config manually configured

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,9 +1,20 @@
 resource "aws_s3_bucket" "s3_bucket_website" {
   bucket = "aaslam-s3-static-bucket-website"
-  
+
 
   tags = {
     Name        = "aaslam-s3-static-bucket-website"
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket_versioning" "s3_versioning_enabled" {
+  bucket = aws_s3_bucket.s3_bucket_website.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+  
+}
+
+
+
