d913d08ad6e0c9b0018a2317e5dfa08b4e48fa10
adamaubds <150913197+adamaubds@users.noreply.github.com>
2024-09-24 10:33:20 +0100

    changes to provider block for ubds laptop

--- providers.tf
+++ providers.tf
@@ -1,12 +1,14 @@
 provider "aws" {
   region  =  var.region
+  profile = "home"
 }
 
 terraform {
   backend "s3" {
     bucket = "terraform-backend-adam-canterill"
-    key    = "s2svpn/terraform.tfstate"
+    key    = "web_app/terraform.tfstate"
     region = "us-east-1"
+    profile = "home"
   }
 }
 
